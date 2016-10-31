14c675c239a44b16cd4328aca6c947038fd1cf51
svoboond <svoboond@gmail.com>
2016-10-31 22:04:27 +0100

    Add category default value (see #6)

--- data/sql/add_pu_columns_PAR.sql
+++ data/sql/add_pu_columns_PAR.sql
@@ -7,4 +7,4 @@ update PAR set PU_PODDELENI_CISLA_PAR = PODDELENI_CISLA_PAR;
 alter table PAR add column PU_VYMERA_PARCELY integer;
 update PAR set PU_VYMERA_PARCELY = VYMERA_PARCELY;
 
-alter table PAR add column PU_KATEGORIE integer;
+alter table PAR add column PU_KATEGORIE integer default 1;
